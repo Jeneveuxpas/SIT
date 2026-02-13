df6483a1532d91debef0bdaa5de4d510f047d154
hanfu <jeneveuxpas@hanfudeMacBook-Air.local>
2026-02-13 21:22:09 +0800

    kv-4.0-repa-0.5

--- launch.sh
+++ launch.sh
@@ -9,7 +9,7 @@
 #  ./launch.sh --config configs/10,12-4,6.yaml --exp-name 10,12-4,6 --gpu 2,3 --num-gpus 2 
 #  ./launch.sh --config configs/attn_mse_repa_early_stop.yaml --exp-name attn_mse_repa_early_stop --gpu 0,1,2 --num-gpus 3 --resume-step 400000
 #  ./launch.sh --config configs/kv-stop-300-500k.yaml --exp-name kv-stop-300-500k --gpu 0,1,2,3 --num-gpus 4 --resume-step 300000
-#  ./launch.sh --config configs/kv-stop-350-500k.yaml --exp-name kv-stop-350-500k --gpu 4,5,6,7 --num-gpus 4 --resume-step 350000
+#  ./launch.sh --config configs/em.yaml --exp-name em --gpu 6,7 --num-gpus 2 --resume-step 0
 
 # ============================================================================
 set -e
